5eba8dbbb1c05406c3efa058dda375af130ee1e6
Oscar Brügger <obrugger@gmail.com>
2015-12-07 21:33:55 +0100

    Had to copy all of snake_server into new project. Did not know how to commit new updates.

--- src/Yo.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author Oscar
- * @since 07-12-2015
- */
-public class Yo  {
-}
---
+++ src/model/Scores.java
@@ -0,0 +1,30 @@
+package model;
+
+/**
+ * Created by Oscar on 25-11-2015.
+ */
+public class Scores{
+
+    private int userId;
+    private int score;
+
+    public Scores(){
+
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
